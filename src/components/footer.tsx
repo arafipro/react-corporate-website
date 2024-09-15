8ecaa6aca19d926d587c269fb2fcfcec82f4a3c8
Hiroki Chiyoda <contact@arafipro.com>
2024-09-15 21:40:26 +0900

    fix: responsive setting fixed
    CSSのxlをlgに変更

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="xl:h-56 h-64 bg-white pl-4 xl:pl-0">
+    <footer className="lg:h-56 h-64 bg-white pl-4 lg:pl-0">
 			<main className="mx-auto container">
-        <div className="xl:flex justify-between items-center pt-10">
+        <div className="lg:flex justify-between items-center pt-10">
           <a href="#">
             <img src="/logo.svg" alt="logo" width="100" height="100" />
           </a>
-          <section className="text-sm pt-2.5 xl:pt-0">
+          <section className="text-sm pt-2.5 lg:pt-0">
             <p>Web Entertainment Design Inc.</p>
             <p>West Building 3F</p>
             <p>9-99 Sakuragaokacho Shibuya-ku</p>
@@ -14,7 +14,7 @@ export default function Footer() {
             <p>T/99-9999-9999</p>
           </section>
         </div>
-        <p className="text-xs pt-10 xl:pt-12">© Web Entertainment Design Inc.</p>
+        <p className="text-xs pt-10 lg:pt-12">© Web Entertainment Design Inc.</p>
       </main>
     </footer>
   );
