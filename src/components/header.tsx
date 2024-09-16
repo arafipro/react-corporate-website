a197674a988e57bd9ae755ca07960429434e98b3
Hiroki Chiyoda <contact@arafipro.com>
2024-09-16 11:29:25 +0900

    fix: change header
    背景色を変更
    SPサイト時のロゴの位置を修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="h-20 flex justify-between items-center fixed top-0 left-0 right-0 bg-[#F5F5F5]/80">
-      <h1 className="w-48 flex justify-end items-end">
+    <header className="h-20 flex justify-between items-center fixed top-0 left-0 right-0 bg-gray-100/80">
+      <h1 className="w-48 lg:flex justify-end items-end pl-4">
         <a href="#">
           <img src="/logo.svg" alt="logo" width="100" height="100" />
         </a>
@@ -21,7 +21,7 @@ export default function Header() {
       </button>
       <ul
         className={`lg:flex ${
-          isMenuOpen ? "flex text-center text-xl bg-[#F5F5F5]/80" : "hidden"
+          isMenuOpen ? "flex text-center text-xl bg-gray-100/80" : "hidden"
         } flex-col lg:flex-row absolute lg:relative top-20 lg:top-0 left-0 w-full lg:w-auto bg-white lg:bg-transparent`}
       >
         <li className="p-4">
