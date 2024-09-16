b905acd832dfc257a02c617ed270def5c77e0948
Hiroki Chiyoda <contact@arafipro.com>
2024-09-16 09:27:26 +0900

    fix: responsive setting add
    w-1/3にlgを追加

--- src/components/ui/news-content.tsx
+++ src/components/ui/news-content.tsx
@@ -8,7 +8,7 @@ export default function NewsContent({
   title: string;
 }) {
   return (
-    <div className="flex flex-col justify-center first:pl-0 space-y-4 lg:border-r-2 border-black last:border-r-0 w-1/3 h-20">
+    <div className="flex flex-col justify-center first:pl-0 space-y-4 lg:border-r-2 border-black last:border-r-0 lg:w-1/3 h-20">
       <div className="flex space-x-2.5">
         <time className="text-sm font-light">{date}</time>
         <p className="flex justify-center items-center text-white text-xs font-light bg-black uppercase w-12">
