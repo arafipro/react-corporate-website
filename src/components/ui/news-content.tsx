4821967caf387b92b5ee5270589a4c9517a817b9
Hiroki Chiyoda <contact@arafipro.com>
2024-09-16 21:25:27 +0900

    feat: create news-page
    ニュースページを作成
    tailwind-mergeをインストール
    NewsContentコンポーネントにtwMergeを導入

--- src/components/ui/news-content.tsx
+++ src/components/ui/news-content.tsx
@@ -1,14 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
 export default function NewsContent({
   date,
   tag,
   title,
+  className,
 }: {
   date: string;
   tag: string;
   title: string;
+  className?: string;
 }) {
   return (
-    <div className="flex flex-col justify-center first:pl-0 space-y-4 lg:border-r-2 border-black last:border-r-0 lg:w-1/3 h-20">
+    <div
+      className={twMerge(
+        `${className}`,
+        "flex flex-col justify-center first:pl-0 space-y-4 lg:w-1/3 h-20"
+      )}
+    >
       <div className="flex space-x-2.5">
         <time className="text-sm font-light">{date}</time>
         <p className="flex justify-center items-center text-white text-xs font-light bg-black uppercase w-12">
--- src/news-page.tsx
+++ src/news-page.tsx
@@ -1,10 +1,55 @@
+import Footer from "./components/footer";
 import Header from "./components/header";
+import NewsContent from "./components/ui/news-content";
 
-export default function NewsPage() {
+export default function News() {
   return (
     <>
       <Header />
-      <main className="max-w-5xl mx-auto mt-20">NewsPage</main>
+      <main className="max-w-5xl mx-auto lg:mt-20 lg:mb-32 px-4">
+        <h2 className="text-4xl font-light uppercase tracking-[0.25em] pt-20">
+          News & Press
+        </h2>
+        <div className="mt-24 border-b border-black">
+          <button className="lg:w-60 w-1/2 font-light pb-5 border-b-2 border-black">
+            ニュース
+          </button>
+          <button className="lg:w-60 w-1/2 font-light pb-5">
+            プレスリリース
+          </button>
+        </div>
+        <NewsContent
+          date="2022.01.01"
+          tag="news"
+          title="タイトルタイトルタイトルタイトル"
+          className="border-r-0 border-b border-black lg:w-full lg:mt-14 mt-10 lg:pb-14 pb-10"
+        />
+        <NewsContent
+          date="2022.01.01"
+          tag="news"
+          title="タイトルタイトルタイトルタイトル"
+          className="border-r-0 border-b border-black lg:w-full lg:mt-14 mt-10 lg:pb-14 pb-10"
+        />
+        <NewsContent
+          date="2022.01.01"
+          tag="news"
+          title="タイトルタイトルタイトルタイトル"
+          className="border-r-0 border-b border-black lg:w-full lg:mt-14 mt-10 lg:pb-14 pb-10"
+        />
+        <NewsContent
+          date="2022.01.01"
+          tag="news"
+          title="タイトルタイトルタイトルタイトル"
+          className="border-r-0 border-b border-black lg:w-full lg:mt-14 mt-10 lg:pb-14 pb-10"
+        />
+        <NewsContent
+          date="2022.01.01"
+          tag="news"
+          title="タイトルタイトルタイトルタイトル"
+          className="border-r-0 border-b border-black lg:w-full lg:mt-14 mt-10 lg:pb-14 pb-10"
+        />
+      </main>
+      <Footer />
     </>
   );
 }
